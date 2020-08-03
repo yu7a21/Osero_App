2bbe0bace90e8d619b09b422e6938da45193bdc7
s16140 <s16140@numazu.kosen-ac.jp>
2020-08-03 11:41:06 +0900

    Player.cppの更新

--- Player.cpp
+++ Player.cpp
@@ -10,19 +10,23 @@ Player::Player(int ID){
 }
 
 void Player::set_PlayerID(int ID){
-  PlayerID = ID;
+  this.PlayerID = ID;
 }
 
 int Player::get_PlayerID(){
-  return PlayerID;
+  return this.PlayerID;
 }
 
-bool Player::playerInput(int N_player){
+bool Player::playerInput(){
   int length,width;
   printf("縦の入力\n");
   cin >> length;
   printf("横の入力\n");
   cin >> width;
+  if(!(0<=length<=7 || 0<=width<=7)){
+    printf("0以上7以下の数字で入力してください\n");
+    return false;
+  }
   changeBoard(length,width);
   return true;
 }
