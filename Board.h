926221cff125c1b799dde87a0f5f735fcf3a0389
s16123 <s16123@numazu.kosen-ac.jp>
2020-08-24 11:36:16 +0900

    [update]Board.h Board.cppにおける修正(IDの引き継ぎ)

--- Board.h
+++ Board.h
@@ -2,6 +2,7 @@
 #define BOARD_H
 
 using namespace std;
+class Player;
 class Board
 {
 private:
@@ -14,12 +15,12 @@ private:
 public:
     Board(/* args */);//盤の初期化(コンストラクタ)
     ~Board();//デストラクタ
-    bool judgePass();//パスの判断 -->judgeInput() ok
+    bool judgePass(Player currentPlayer);//パスの判断 -->judgeInput() ok
     void dispBoard();//盤の状態を表示 ok
-    void changeBoard(int length,int width);//入力を反映 ok
-    void changeColor();//色をひっくり返す　ok
+    void changeBoard(int length,int width,int id);//入力を反映 ok
+    void changeColor(Player currentPlayer);//色をひっくり返す　ok
     void countStone();//石を数える ok
-    bool judgeInput();//引数のマスにおけるか判断 ok
+    bool judgeInput(Player currentPlayer);//引数のマスにおけるか判断 ok
 };
 
 #endif
